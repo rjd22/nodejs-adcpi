8fa0e4f1fe831e34d2732983968eeadca8b68347
Robert-Jan de Dreu <robertjandreu@gmail.com>
2015-06-22 00:01:57 +0200

    Added conversion to voltage example

--- main.js
+++ main.js
@@ -4,9 +4,13 @@ var address = 0x68;
 var gain = 0; //{0,1,2,3} represents {x1,x2,x4,x8}
 var resolution = 3; //{0,1,2,3} and represents {12,14,16,18} bits
 
+var pga = 0.5; // {x1,x2,x4,x8}gain represents {0.5, 1, 2, 4}
+var lsb = 0.0005; // {12,14,16,18}bits {0.0005, 0.000125, 0.00003125, 0.0000078125}lsb
+
 var mcp = new mcp3424(address, gain, resolution, '/dev/i2c-1');
 
-setTimeout(function(){
-    console.log(mcp.getMv(0)); //for channel 0
-    console.log(mcp.getMv(3)); //for channel 3
-}, 2000); //first conversion needs a bit time...(smaller resolution -> faster)
+setInterval(function(){
+    var voltage = ((mcp.getMv(0) * (lsb / pga)) * 2.471);
+    console.log('Reading: ' + voltage);
+}, 1000);
+
